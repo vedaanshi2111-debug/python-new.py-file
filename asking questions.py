6816523bd733e85292ad6f4bc239d5b4d95caf73
vedaanshi2111-debug <vedaanshi2111@gmail.com>
2026-01-16 22:18:39 +0530

    asking and add more questions

---
+++ asking questions.py
@@ -0,0 +1,24 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/hp/Desktop/new folder new/python-new.py-file/python-new.py-file/asking questions.py
+What is your name? vedaanshi verma
+G'day,  vedaanshi verma
+>>> 
+= RESTART: C:/Users/hp/Desktop/new folder new/python-new.py-file/python-new.py-file/asking questions.py
+What is your name? vedaanshi verma
+how are you,  vedaanshi verma
+>>> 
+= RESTART: C:/Users/hp/Desktop/new folder new/python-new.py-file/python-new.py-file/asking questions.py
+What is your hobby? singing
+nice,  singing
+>>> 
+= RESTART: C:/Users/hp/Desktop/new folder new/python-new.py-file/python-new.py-file/asking questions.py
+What is your favorite  food? fish curry
+great,  fish curry
+>>> 
+= RESTART: C:/Users/hp/Desktop/new folder new/python-new.py-file/python-new.py-file/asking questions.py
+Which type of movie do you like honestly
+= RESTART: C:/Users/hp/Desktop/new folder new/python-new.py-file/python-new.py-file/asking questions.py
+Which type of movie do you like honestly?comedy mostly
+great choice,  comedy mostly
